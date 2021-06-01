5eae9f76e948498f59890e0bd3d0397a4dfcb64e
Ghost <vladimirvsevolodovi@yandex.ru>
2021-06-01 20:37:41 +0300

    git: add src/getNPMPaths functionality

---
+++ src/getNPMPaths.ts
@@ -0,0 +1,19 @@
+import nodePath = require("path");
+
+let paths: string = "";
+
+if(process.platform.startsWith("win")) {
+
+
+
+} else if(process.platform.includes("linux") && process.execPath.includes(".nvm")) {
+
+	paths = nodePath.join( process.execPath, "../../lib/node_modules/npm/lib/" );
+
+} else if(process.platform.includes("linux")) {
+
+	paths = nodePath.join( process.execPath, "/usr/lib/node_modules/npm/lib/node_modules/npm/lib/" );
+
+}
+
+export = paths;
